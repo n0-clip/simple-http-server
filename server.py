7a50e3c15eac7efbe9597aaf921992e6ae294364
Daz <122198240+payloadpanda@users.noreply.github.com>
2023-06-01 17:58:22 +0100

    Added handling for POST requests

--- server.py
+++ server.py
@@ -3,7 +3,17 @@ import socketserver
 
 PORT = 8000
 
-Handler = http.server.SimpleHTTPRequestHandler
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        print(post_data)
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'POST request received')
+
+Handler = MyHttpRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
