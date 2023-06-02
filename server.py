b9532f22b9317629721aa2fb827967b323630591
Daz <122198240+payloadpanda@users.noreply.github.com>
2023-06-02 13:11:15 +0100

    Added logging for GET and POST requests

--- server.py
+++ server.py
@@ -1,20 +1,24 @@
 import http.server
 import socketserver
+import logging
 
 PORT = 8000
 
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        http.server.SimpleHTTPRequestHandler.do_GET(self)
+
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        print(post_data)
-
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'POST request received')
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                     str(self.path), str(self.headers), post_data.decode('utf-8'))
+        http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 Handler = MyHttpRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
+    logging.basicConfig(level=logging.INFO)
+    logging.info("serving at port %s", PORT)
     httpd.serve_forever()
