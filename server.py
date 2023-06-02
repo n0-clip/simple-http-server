0e55ac8e96e57a5ac0aafbb8961594da122cf7c8
Daz <122198240+payloadpanda@users.noreply.github.com>
2023-06-02 14:28:03 +0100

    Added POST request echo

--- server.py
+++ server.py
@@ -10,11 +10,22 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
         http.server.SimpleHTTPRequestHandler.do_GET(self)
 
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                     str(self.path), str(self.headers), post_data.decode('utf-8'))
-        http.server.SimpleHTTPRequestHandler.do_GET(self)
+    content_length = int(self.headers['Content-Length'])  # Get the size of data
+    post_data = self.rfile.read(content_length)  # Get the data itself
+    logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                 str(self.path), str(self.headers), post_data.decode('utf-8'))
+
+    # Begin the response
+    self.send_response(200)
+    self.send_header('Content-type', 'text/html')
+    self.end_headers()
+
+    # Send the html message
+    response = BytesIO()
+    response.write(b'This is POST request. ')
+    response.write(b'Received: ')
+    response.write(post_data)
+    self.wfile.write(response.getvalue())
 
 Handler = MyHttpRequestHandler
 
